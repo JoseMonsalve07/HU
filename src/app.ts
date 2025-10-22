7aee0b91ddb23ad0037eb3610e6a1669ae71aded
JoseMonsalve07 <00jsmonsalve@gmail.com>
2025-10-22 12:36:37 -0500

    feat: Implementar modelo y servicio de autenticación

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import sequelize from "./dbconfig/database.ts";
 import "dotenv/config";
 import "./models/associations.ts";
 import { runSeeds } from "./seeders/seed.ts";
+import router from "./routes/index.ts";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(router);
 
 sequelize.sync({ alter: true})
     .then(async () => {
